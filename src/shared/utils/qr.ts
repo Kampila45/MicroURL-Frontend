39f73005641055ebe5ddfc64478cd1c5b05c8b7b
Nyasha Kampila <157101318+Kampila45@users.noreply.github.com>
2025-12-22 20:24:23 +0200

    Update qr.ts

--- src/shared/utils/qr.ts
+++ src/shared/utils/qr.ts
@@ -1,10 +1,9 @@
 import QRCode from "qrcode";
 
 export async function generateQRCodeDataURL(url: string): Promise<string> {
-  return QRCode.toDataURL(url, {
+  return await QRCode.toDataURL(url, {
     errorCorrectionLevel: "M",
     type: "image/png",
-    quality: 0.92,
     margin: 1,
     width: 256,
   });
